9803e53dd67461101b0a6fd89a79dfb39bb99dac
Allan Veras <allanverasce@gmail.com>
2025-08-27 16:07:09 -0300

    Update exercicio1_mod02.py

--- exercicios/exercicio1_mod02.py
+++ exercicios/exercicio1_mod02.py
@@ -1,1 +1,16 @@
+## Você pode ter feito totalmente diferente, isto é apenas uma forma.
+
+dna = input("Digite uma sequência de DNA: ").upper()
+motivo = input("Digite o motivo a ser buscado: ").upper()
+
+posicoes = []
+
+for i in range(len(dna) - len(motivo) + 1):
+    if dna[i:i+len(motivo)] == motivo:
+        posicoes.append(i)
+
+if posicoes:
+    print("Motivo encontrado nas posições:", posicoes)
+else:
+    print("Motivo não encontrado na sequência.")
 
